b5649343758bdfb28114bdd4fe4721a12336a04a
Richard <richwats@cisco.com>
2022-03-17 17:39:50 +1100

    try lifecycle

--- modules/terraform-intersight-hx/modules/node_profile/main.tf
+++ modules/terraform-intersight-hx/modules/node_profile/main.tf
@@ -14,8 +14,44 @@ data "intersight_compute_physical_summary" "this" {
 }
 
 locals {
+  netmask2cidr = {
+    "0.0.0.0"         = "0"
+    "128.0.0.0"       = "1"
+    "192.0.0.0"       = "2"
+    "224.0.0.0"       = "3"
+    "240.0.0.0"       = "4"
+    "248.0.0.0"       = "5"
+    "252.0.0.0"       = "6"
+    "254.0.0.0"       = "7"
+    "255.0.0.0"       = "8"
+    "255.128.0.0"     = "9"
+    "255.192.0.0"     = "10"
+    "255.224.0.0"     = "11"
+    "255.240.0.0"     = "12"
+    "255.248.0.0"     = "13"
+    "255.252.0.0"     = "14"
+    "255.254.0.0"     = "15"
+    "255.255.0.0"     = "16"
+    "255.255.128.0"   = "17"
+    "255.255.192.0"   = "18"
+    "255.255.224.0"   = "19"
+    "255.255.240.0"   = "20"
+    "255.255.248.0"   = "21"
+    "255.255.252.0"   = "22"
+    "255.255.254.0"   = "23"
+    "255.255.255.0"   = "24"
+    "255.255.255.128" = "25"
+    "255.255.255.192" = "26"
+    "255.255.255.224" = "27"
+    "255.255.255.240" = "28"
+    "255.255.255.248" = "29"
+    "255.255.255.250" = "30"
+    "255.255.255.252" = "31"
+    "255.255.255.255" = "32"
+  }
   # data_ip_range = data.intersight_hyperflex_cluster_profile.this.results[0].data_ip_range # not working?
   # data_ip_start_addr = regex(local.data_ip_range.start_addr
+  # data_cidr = format("%s/%")
 
   # hxdp_data_ip = var.hxdp_data_ip == null ? (local.data_ip_range.start_addr) : var.hxdp_data_ip
 }
@@ -40,5 +76,17 @@ resource "intersight_hyperflex_node_profile" "this" {
 
   cluster_profile {
     moid = var.cluster_moid
+  }
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to IP address as Intersight will set these if not defined
+      hxdp_data_ip,
+      hxdp_mgmt_ip,
+      hxdp_storage_client_ip,
+      hypervisor_control_ip,
+      hypervisor_data_ip,
+      hypervisor_mgmt_ip
+    ]
   }
 }

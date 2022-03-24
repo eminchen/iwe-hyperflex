a716afa6dd510fa3822f52ea90a4bb5a266a2931
Richard <richwats@cisco.com>
2022-03-24 14:19:38 +1100

    retry no-op

--- iwe-example.tf
+++ iwe-example.tf
@@ -28,7 +28,7 @@ module "hx" {
   source = "./modules/terraform-intersight-hx"
 
   ### COMMON SETTINGS ###
-  action              = "Unassign" # Validate, Deploy, Continue, Retry, Abort, Unassign, No-op
+  action              = "No-op" # Validate, Deploy, Continue, Retry, Abort, Unassign, No-op
   wait_for_completion = false
   organization        = "default"
   tags                = []
